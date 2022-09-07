5d1e7587ce117b46398400fe782c528a01c8eff4
Nikolui333 <56168199+Nikolui333@users.noreply.github.com>
2022-09-07 17:17:40 +0300

    added dependencies

---
+++ app/src/main/java/com/sem/receivedata/presrntation/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.sem.receivedata.presrntation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.sem.receivedata.R
+import com.sem.receivedata.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
