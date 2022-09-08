b1514e091b1559a82c059106d48d7eacaaa08606
Nikolui333 <56168199+Nikolui333@users.noreply.github.com>
2022-09-08 09:20:07 +0300

    added new models

---
+++ app/src/main/java/com/sem/receivedata/data/models/PaginationApiModel.kt
@@ -0,0 +1,11 @@
+package com.sem.receivedata.data.models
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class PaginationApiModel {
+
+    @SerializedName("result") @Expose
+    val result: ResultApiModel? = null
+
+}
---
+++ app/src/main/java/com/sem/receivedata/data/models/ResultApiModel.kt
@@ -0,0 +1,11 @@
+package com.sem.receivedata.data.models
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class ResultApiModel {
+
+    @SerializedName("data") @Expose
+    val data: Data? = null
+
+}
